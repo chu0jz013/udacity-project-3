7db161572b792ff878d45b9dbc56182cf4aca984
William Kieu <williamkieu-devops@outlook.com>
2024-10-26 10:49:48 +0700

    [Add] env build phase

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
-CLUSTER_NAME=$1
-REGION=$2
-eksctl create cluster --name "$CLUSTER_NAME" --region="$REGION" --nodes-min=2 --nodes-max=3
-aws eks update-kubeconfig --region "$REGION" --name "$CLUSTER_NAME"
+
+# Hardcoded values
+CLUSTER_NAME="project3-udacity"
+REGION="us-east-1"
+PROFILE="udacity"
+
+eksctl create cluster --name "$CLUSTER_NAME" --region "$REGION" --nodes-min=2 --nodes-max=3 --profile "$PROFILE"
+aws eks update-kubeconfig --region "$REGION" --name "$CLUSTER_NAME" --profile "$PROFILE"
